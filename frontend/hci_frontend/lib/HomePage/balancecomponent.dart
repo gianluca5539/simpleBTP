77b593784d56aca1edd60a787ad008c00b81ad06
Gianluca Monte <gianluca.monte02@gmail.com>
2024-04-17 18:31:36 +0200

    balance component

--- frontend/hci_frontend/lib/HomePage/balancecomponent.dart
+++ frontend/hci_frontend/lib/HomePage/balancecomponent.dart
@@ -1,8 +1,67 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:hci_frontend/assets/colors.dart';
 
 class BalanceComponent extends StatelessWidget {
+  late double balance;
+  late double variation;
+
+  BalanceComponent({super.key, required this.balance, required this.variation});
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('test'));
+    return Padding(
+      padding: const EdgeInsets.only(top: 40.0),
+      child: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 3,
+                  offset: const Offset(0, 3),
+                ),
+              ]),
+          width: 0.9 * MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+            child: Column(
+              // align text to the left
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 17.0, right: 17.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Il tuo investimento",
+                        style: TextStyle(color: textColor, fontSize: 24),
+                      ),
+                      Text(
+                        "${variation > 0 ? "▲" : "▼"}$variation%",
+                        style:
+                            const TextStyle(color: primaryColor, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 17.0, right: 17.0),
+                  child: Text(
+                    "€${balance.toStringAsFixed(2).replaceAll(".", ",").replaceAllMapped(RegExp(r"(\d{1,3})(?=(\d{3})+(?!\d))"), (Match m) => "${m[1]}.")}",
+                    style: const TextStyle(color: textColor, fontSize: 36),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- frontend/hci_frontend/lib/HomePage/homepage.dart
+++ frontend/hci_frontend/lib/HomePage/homepage.dart
@@ -10,11 +10,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // off white background
+      backgroundColor: Color.fromARGB(255, 248, 248, 248),
       appBar: AppTopBar('BTP Genius'),
       // add a body and a footer
       body: Column(
         children: [
-          BalanceComponent(),
+          BalanceComponent(balance: 131231.22, variation: 1.12),
         ],
       ),
       bottomNavigationBar: Footer('home'),
